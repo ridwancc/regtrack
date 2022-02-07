31ffed81f45983978e7df575a3a10ebf4bffe607
Ridwan Chowdhury <ridwanc@icloud.com>
2022-02-07 00:45:02 +0000

    draw number plate image to canvas

--- src/components/imageProcessing.svelte
+++ src/components/imageProcessing.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { drawStretched, calculateGeometry } from "./../services/stretch.js";
   export let canvas;
   export let buffer;
   export let video;
@@ -19,9 +20,11 @@
     return null;
   };
 
+  const image = new Image();
+  image.src = "/images/front-plate.jpg";
+
   beforeUpdate(() => {
     video.addEventListener("loadeddata", async () => {
-
       window.scrollTo(0, document.body.scrollHeight);
       const ctxBuffer = buffer.getContext("2d");
       const ctxOutput = canvas.getContext("2d");
@@ -35,9 +38,20 @@
         const imageData = ctxBuffer.getImageData(0, 0, width, height);
         // send data to the worker thread
         const plate = await detectPlate(imageData, vrn);
+        // clear the output canvas
+        ctxOutput.clearRect(0, 0, width, height);
 
-        if (plate) {
-          ctxOutput.putImageData(plate.imageData, 0, 0, 0, 0, width, height);
+        if (plate && plate.points && plate.points.length > 3) {
+          // ctxOutput.putImageData(plate.imageData, 0, 0, 0, 0, width, height);
+          ctxOutput.drawImage(video, 0, 0, width, height);
+          const geometry = calculateGeometry(
+            image,
+            { x: plate.points[3].x, y: plate.points[3].y },
+            { x: plate.points[0].x, y: plate.points[0].y },
+            { x: plate.points[1].x, y: plate.points[1].y },
+            { x: plate.points[2].x, y: plate.points[2].y }
+          );
+          drawStretched(ctxOutput, image, geometry, false);
         }
 
         if (video.paused || video.ended) {
@@ -49,7 +63,7 @@
           frame = requestAnimationFrame(draw);
         }
       };
-      
+
       // start the animation
       let frame = requestAnimationFrame(draw);
     });
