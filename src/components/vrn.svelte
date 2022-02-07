48a5664e475ad4039a9fa9e7c8626ec1bc4ed9f7
Ridwan Chowdhury <ridwanc@icloud.com>
2022-02-07 16:54:57 +0000

    add custom vrn input

--- src/components/vrn.svelte
+++ src/components/vrn.svelte
@@ -1,26 +1,44 @@
 <script>
-    export let vrn = '';
-    
-    const handleChange = (e) => {
-        vrn = e.target.value.toUpperCase();
-    }
+  export let vrn = "";
+  export let placement = "front";
+
+  const handleSelect = (e) => {
+      placement = e.target.value;
+  }
+
+  const handleChange = (e) => {
+    vrn = e.target.value.toUpperCase();
+  };
 </script>
 
 <div class="container mt-4">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <div class="card">
-                <input maxLength="7" type="text" placeholder="Enter your VRN" class="text-center text-uppercase pt-2 active" value={vrn} on:change={(e) => handleChange(e)}>
-            </div>
-        </div>
+  <div class="row">
+    <input
+      maxLength="7"
+      type="text"
+      placeholder="Enter your VRN"
+      class="text-center text-uppercase pt-2 active"
+      value={vrn}
+      on:change={e => handleChange(e)}
+    />
+    <div class="placement-row mt-2">
+      <label>
+        Front:
+        <input type="radio" group={placement} name="placement" value="front" on:change={e => handleSelect(e)} checked/>
+      </label>
+      <label>
+        Back:
+        <input type="radio" group={placement} name="placement" value="back" on:change={e => handleSelect(e)} />
+      </label>
     </div>
+  </div>
 </div>
 
 <style>
-    .active {
-        font-size: larger;
-        background:#eed85b;
-        font-weight: 600;
-        letter-spacing: 0.3rem;
-    }
+  .active {
+    font-size: larger;
+    background: #eed85b;
+    font-weight: 600;
+    letter-spacing: 0.3rem;
+  }
 </style>
